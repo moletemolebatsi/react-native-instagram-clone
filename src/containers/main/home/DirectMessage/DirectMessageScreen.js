d7d68deb20d9655cff4ae44329fe5a3e8acd8d72
moletemolebatsi <43677651+moletemolebatsi@users.noreply.github.com>
2024-08-25 09:46:51 +0200

    Update DirectMessageScreen.js

--- src/containers/main/home/DirectMessage/DirectMessageScreen.js
+++ src/containers/main/home/DirectMessage/DirectMessageScreen.js
@@ -5,7 +5,7 @@ import Title from './Title';
 import MessagesList from './MessagesList';
 import {FlatList, TouchableOpacity} from 'react-native-gesture-handler';
 import colors from '../../../../res/colors';
-import images from '../../../../../res/images';
+import images from '../../../../res/images';
 
 const data = [{key: '1'}];
 
