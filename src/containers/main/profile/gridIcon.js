ba3d6c1d587116d59db6a80034976ddbbd380ede
moletemolebatsi <43677651+moletemolebatsi@users.noreply.github.com>
2024-08-25 10:37:44 +0200

    Update gridIcon.js

--- src/containers/main/profile/gridIcon.js
+++ src/containers/main/profile/gridIcon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Image, View} from 'react-native';
-import images from 'res/images';
+import images from '../../../res/images';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
 export default function GridIcon() {
