c3c3bc164572bc563bdc2845af00a58dbc1a1b0b
moletemolebatsi <43677651+moletemolebatsi@users.noreply.github.com>
2024-08-25 09:44:40 +0200

    Update activityScreen.js

--- src/containers/main/activity/activityScreen.js
+++ src/containers/main/activity/activityScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import palette from '../../../../res/palette';
+import palette from '../../../res/palette';
 
 export default function activityScreen() {
   return (
