6c6cef62c8a85bcc74652e8ddef5a93f2c421dff
moletemolebatsi <43677651+moletemolebatsi@users.noreply.github.com>
2024-08-25 09:34:52 +0200

    Update addPostNavigator.js

--- src/containers/main/addPost/addPostNavigator.js
+++ src/containers/main/addPost/addPostNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import addPostScreen from './addPostScreen';
-import colors from 'res/colors';
+import colors from '../../../../res/colors';
 import {View, Text} from 'react-native';
 
 export default function addPostNavigator() {
