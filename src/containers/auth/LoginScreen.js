90fabc9f7e1584883b7c05145a92fc4e3b3c7228
moletemolebatsi <43677651+moletemolebatsi@users.noreply.github.com>
2024-08-27 15:34:59 +0200

    Update LoginScreen.js

--- src/containers/auth/LoginScreen.js
+++ src/containers/auth/LoginScreen.js
@@ -18,25 +18,7 @@ export default function LoginScreen({_signInAsync}) {
           style={{height: 70, resizeMode: 'contain'}}
         />
       </View>
-      <View style={Styles.userNameContainer}>
-        <TextInput
-          style={Styles.userNameInput}
-          placeholder="Telefon numarası, kullanıcı adı veya e-posta"
-        />
-      </View>
-      <View style={Styles.passwordContainer}>
-        <TextInput style={Styles.passwordInput} placeholder="Şifre" />
-      </View>
-      <View style={Styles.forgotPasswordContainer}>
-        <TouchableOpacity>
-          <Text style={Styles.forgotPasswordText}>Şifreni mi unuttun?</Text>
-        </TouchableOpacity>
-      </View>
-      <TouchableOpacity
-        style={Styles.loginContainer}
-        onPress={() => _signInAsync}>
-        <Text style={Styles.loginText}>Giriş Yap</Text>
-      </TouchableOpacity>
+
     </View>
   );
 }
