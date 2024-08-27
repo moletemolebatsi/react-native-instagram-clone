60f026a6424c1de0845b55bbd33ca582f3ee0e2b
moletemolebatsi <43677651+moletemolebatsi@users.noreply.github.com>
2024-08-27 14:41:40 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 function App() {
- // return <NavigationContainer>{<AppNavigator />}</NavigationContainer>;
+  return <NavigationContainer>{<AppNavigator />}</NavigationContainer>;
 }
 
 export default App;
