a6cfa563a492dfe3c1cd6c40e3c0664c7db8d089
moletemolebatsi <43677651+moletemolebatsi@users.noreply.github.com>
2024-08-27 09:56:51 +0200

    Update StoryListItem.js

--- src/components/StoryListItem.js
+++ src/components/StoryListItem.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import {View, Image, Text, TouchableOpacity, StyleSheet} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
 
 export default function StoryListItem({item, storyOnPress, name}) {
   return (
     <View style={Styles.container}>
       <TouchableOpacity onPress={storyOnPress}>
-        <LinearGradient
-          colors={['#CA1D7E', '#E35157', '#F2703F']}
-          start={{x: 0.0, y: 1.0}}
-          end={{x: 1.0, y: 1.0}}
-          style={{borderRadius: 100, padding: 2}}>
-          <View style={{borderWidth: 2, borderRadius: 100}}>
+        
+    <View style={{borderWidth: 2, borderRadius: 100}}>
             <Image
               source={{uri: 'https://picsum.photos/200'}}
               style={{width: 55, height: 55, borderRadius: 70}}
             />
           </View>
-        </LinearGradient>
+        
       </TouchableOpacity>
       <View>
         <Text style={Styles.storyText}>{name}</Text>
